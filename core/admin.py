f1e5fca39d654eb9beb0cc99e16927bd1ab1fa67
Ibrahim Abdulqoyyum <ibrahimabdulqoyyum@gmail.com>
2022-01-20 23:39:20 +0100

    Created new models, Configured database

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Room, Topic
 
-# Register your models here.
+
+admin.site.register(Room)
+admin.site.register(Topic)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Room(models.Model):
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey('Topic', on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateField(auto_now_add=True)
+
+class Topic(models.Model):
+    topic = models.CharField(max_length=255)
