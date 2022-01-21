84193c501554bf82c22ffa80e5d76c8810df6a81
Ibrahim Abdulqoyyum <ibrahimabdulqoyyum@gmail.com>
2022-01-21 23:25:52 +0100

    Few changes

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('room/<str:pk>/', views.room, name='room'),
 ]
--- core/views.py
+++ core/views.py
@@ -6,3 +6,8 @@ def index(request):
     rooms = Room.objects.all()
     context = {'rooms':rooms}
     return render(request, 'core/index.html', context)
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room':room}
+    return render(request, 'core/room.html', context)
