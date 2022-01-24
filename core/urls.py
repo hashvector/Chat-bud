179e26303e2a7661ef0a17019241bf2583c5c160
hashvector <71531531+hashvector@users.noreply.github.com>
2022-01-24 21:07:17 +0100

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('room/<str:pk>/', views.room, name='room'),
     path('edit-room/<str:pk>/', views.editRoom, name='edit-room'),
+    path('delete-room/<str:pk>/', views.deleteRoom, name='delete-room'),
     path('create-room', views.createRoom, name='create-room'),
 ]
