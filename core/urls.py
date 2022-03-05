0f843e2bcd4126b0c56dd939086fb9ffa2aea4c0
Ibrahim Abdulqoyyum <ibrahimabdulqoyyum@gmail.com>
2022-03-05 04:00:16 +0100

    new commit

--- core/urls.py
+++ core/urls.py
@@ -5,10 +5,13 @@ urlpatterns = [
     path('login/', views.loginUser, name='login'),
     path('register/', views.registerUser, name='register'),
     path('logout/', views.logoutUser, name='logout'),
+
     path('', views.index, name='index'),
     path('room/<str:pk>/', views.room, name='room'),
+
     path('create-room', views.createRoom, name='create-room'),
     path('edit-room/<str:pk>/', views.editRoom, name='edit-room'),
     path('deleteroom/<str:pk>/', views.deleteRoom, name='delete-room'),
+    
     path('deletemessage/<str:pk>/', views.deleteMessage, name='delete-message'),
 ]
