a4aef7c4cba7002dc4904b3a30a2c4cb40c4f667
hashvector <71531531+hashvector@users.noreply.github.com>
2022-03-09 05:24:06 +0100

    Update settings.py

--- chatbud/settings.py
+++ chatbud/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&h+-+3w#-6x__081)!(@!320l5%x8tw%cvz21-2lrw%-sy_l!2'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
